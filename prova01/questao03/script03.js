62390fbdb46177d1cdb6aec8031b3958b07ff1b5
marceloleda <marcelo.leda@clicksoft.com.br>
2023-09-13 09:59:34 -0400

    questao03 e 04 e 05

---
+++ prova01/questao03/script03.js
@@ -0,0 +1,10 @@
+function soma(){
+    button.addEventListener('click', ()=>{
+        alert("teste");
+      })
+    const numero = prompt("Digite um número");
+    const outroNumero = prompt("Digite outro número");
+
+    const resultado = numero + outroNumero
+    return resultado
+}
